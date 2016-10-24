0242ebe22ba91f6169c2bdca6491f3060d88a308
seojungwon <garden715@gmail.com>
2016-10-24 15:00:04 +0900

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by seojungwon on 2016. 10. 24..
+//  Copyright © 2016년 seojungwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by seojungwon on 2016. 10. 24..
+//  Copyright © 2016년 seojungwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
